821ad85dd60b3c120c6442fdc7682bf8bae80913
dreamlin517 <169177274@qq.com>
2018-09-16 22:18:01 +0800

    初始化vue项目

---
+++ src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    hello world
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import fastClick from 'fastclick';
+import App from './App';
+import router from './router';
+import './assets/css/reset.css';
+import './assets/css/border.css';
+
+Vue.config.productionTip = false;
+fastClick.attach(document.body);
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>',
+});
